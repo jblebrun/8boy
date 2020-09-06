d741c614a4b2659aa85b15c9c84d0de841a5b606
jblebrun <jblebrun@gmail.com>
2020-09-05 23:22:42 -0700

    New loader screen with context (#14)

--- arduboy-chip8.ino
+++ arduboy-chip8.ino
@@ -57,7 +57,6 @@ void runEmu() {
         return;
     }
 
-    
     unsigned long t = micros();
     if(t < next) {
         return;
@@ -99,48 +98,88 @@ void runEmu() {
 
 }
 
-void runLoader() {
+// Print out 5 program names, or all of them if less than 5. 
+// If there are less than 5, the existing ones are weighted towards the top.
+// Display a '>' cursor next to the one that's considered selected.
+void printMenu() {
     boy.clear();
     boy.setCursor(0,0);
-    super = pgm_read_byte(&programs[pidx].super);
-    boy.print(F("Select: "));
-    boy.setCursor(0,30);
-    boy.print(pidx);
-    boy.setCursor(16,30);
+
     char buffer[16];
-    strcpy_P(buffer, (const char*)pgm_read_ptr(&programs[pidx].name));
-    boy.println(buffer);
+
+    // our window shows up to 5 items:
+    // current, 2 above, 2 below
+    // If we're pointing at 0-2, our window starts from item 0. Otherwise it
+    // starts from 2 before where we're pointing.
+    // Moving down like this:
+    // >Item 1   Item 1   Item 1   Item 2
+    //  Item 2  >Item 2   Item 2   Item 3
+    //  Item 3   Item 3  >Item 3  >Item 4
+    //  Item 4   Item 4   Item 4   Item 5
+    //  Item 5   Item 5   Item 5   Item 6
+    uint8_t startIdx = pidx < 2 ? 0 : pidx - 2;
+
+    // If we're near the end, we should just show the last 5 programs.
+    // The cursor will finally progress to the last:
+    //  Item N-4   Item N-4   Item N-4
+    //  Item N-3   Item N-3   Item N-3
+    // >Item N-2   Item N-2   Item N-2
+    //  Item N-1  >Item N-1   Item N-1
+    //  Item N     Item N    >Item N
+    if(startIdx + 5 > PROGRAM_COUNT) startIdx = PROGRAM_COUNT - 5;
+
+    // If it's a short list, just show everything.
+    if(PROGRAM_COUNT <= 5) startIdx = 0;
+
+    // Weighted towards the top, so a short list is like:
+    // |------------
+    // |Item 1
+    // |item 2
+    // |
+    // | 
+    // |
+    // |
+    // |TYPE
+    // |INFO
+    // |-------------
+    for(uint8_t i = 0; i < 5; i++) {
+        uint8_t lidx = i + startIdx;
+        if(lidx < PROGRAM_COUNT) {
+            buffer[0] = lidx == pidx ? '>' : ' ';  
+            strncpy_P(&buffer[1], (const char*)pgm_read_ptr(&programs[lidx].name), 16);
+            boy.println(buffer);
+            
+        } else {
+            boy.println("");
+        }
+    }
+
+    // Space blank line.
+    boy.println("");
+
+    // Type of program.
+    super = pgm_read_byte(&programs[pidx].super);
     if(super) {
         boy.println(F("SCHIP-8"));
     } else {
         boy.println(F("CHIP-8"));
     }
+
+    // Info from program, if any was included.
     strcpy_P(buffer, (const char*)pgm_read_ptr(&programs[pidx].info));
     boy.println(buffer);
     boy.display();
+}
 
-    boy.pollButtons();
-    if(boy.justPressed(DOWN_BUTTON)) {
-        pidx++;
-        if(pidx >= PROGRAM_COUNT) {
-            pidx = 0;
-        }
-    }
-    if(boy.justPressed(UP_BUTTON)) {
-        pidx--;
-        if(pidx >= PROGRAM_COUNT) {
-            pidx = PROGRAM_COUNT-1;
-        }
-    }
-    if(boy.justPressed(A_BUTTON)) {
-        program = &programs[pidx];
+void loadCurrentItem() {
+  program = &programs[pidx];
         uint16_t size = pgm_read_word(&(program->size));
         const uint8_t* code = pgm_read_ptr(&(program->code));
         render.setKeyMap(
-            pgm_read_byte(&(program->keymap[0])),
-            pgm_read_byte(&(program->keymap[1])),
-            pgm_read_byte(&(program->keymap[2]))
-        );
+                pgm_read_byte(&(program->keymap[0])),
+                pgm_read_byte(&(program->keymap[1])),
+                pgm_read_byte(&(program->keymap[2]))
+                );
         memory.load(code, size);
 
         // Wait for button release before starting emulator, to avoid 
@@ -148,7 +187,21 @@ void runLoader() {
         while(boy.pressed(A_BUTTON));
 
         emu.Reset();
-    }
+}
+
+// This runs once per device loop, while in the game loading phase, to update
+// the display and check inputs.
+void runLoader() {
+    // Show the menu, reflecting the current selection state.
+    printMenu();
+
+    // Handle moving up and own in the menu.
+    boy.pollButtons();
+    if(boy.justPressed(DOWN_BUTTON) && pidx < PROGRAM_COUNT - 1) pidx++; 
+    if(boy.justPressed(UP_BUTTON) && pidx > 0) pidx--;
+
+    // Handle selection.
+    if(boy.justPressed(A_BUTTON|B_BUTTON)) loadCurrentItem();
 }
 
 void loop() {
