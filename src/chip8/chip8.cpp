be800759e12115297125f67ab14e310d002f7174
Jason LeBrun <jblebrun@gmail.com>
2020-10-08 23:04:03 -0700

    Check all of the buttons; prevent spinlock

--- src/chip8/chip8.cpp
+++ src/chip8/chip8.cpp
@@ -40,8 +40,8 @@ inline void Chip8::handleButtons() {
     if (mState.AwaitingKey && mState.Buttons)  {
         // Find the first pressed button, lower hex value gets priority.
         uint8_t pressed = 0;
-        uint8_t mask = 0x01;
-        while((mState.Buttons & mask) == 0) {
+        uint16_t mask = 0x01;
+        while(mask && ((mask & mState.Buttons) == 0)) {
             mask <<= 1;
             pressed++;
         }
